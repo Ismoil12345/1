dcefa63ecce38e57153bf403aaacfda5048d07ef
Ismoil12345 <restartisa66@gmail.com>
2024-03-15 13:52:52 +0500

    first

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-1&2y$s@j3@r2gp-jj9qa(p*$v2iv&zxahy2$41w-!!lg7f=2x)')
+SECRET_KEY = os.environ.get('SECRET_KEY', '83e47069673c89e743b3cfc1e7180268')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
